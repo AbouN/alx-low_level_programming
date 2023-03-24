00bc3143d66a644e58c6a08e2c72dd22919c736a
AbouN <abounyangado@gmail.com>
2023-03-24 08:25:34 -0700

    3. Leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,8 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ *Return: largest number 
+ *
  */
 
 int largest_number(int a, int b, int c)
@@ -26,4 +27,23 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
+}
+/**
+ **main - prints the largest of th 3 integers
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
 }
