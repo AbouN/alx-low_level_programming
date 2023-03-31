9bab2bd097593a7f605a49d41aae031056bcd27a
AbouN <abounyangado@gmail.com>
2023-03-31 05:45:28 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *rot13 -> Encodes a string using rot13
+ * @str: input string
+ * Return: coded
+ */
+
+char *rot13(char *str)
+{
+	char *coded = (char *) malloc(strlen(str) + 1);
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		char s = str[i];
+
+		if (s >= 'a' && s <= 'z')
+		{
+			s = (s - 'a' + 13) % 26 + 'a';
+		}
+		else if (s >= 'A' && s <= 'Z')
+		{
+			s = (s - 'A' + 13) % 26 + 'A';
+		}
+		coded[i] = s;
+	}
+	coded[i] = '\0';
+	return (coded);
+}
