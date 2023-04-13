a3b0040df1acdd17658f7ce459e9de40459eaaed
AbouN <abounyangado@gmail.com>
2023-04-12 18:36:22 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *) malloc(sizeof(int) * width);
-		if (arr[i] == NUll)
+		if (arr[i] == NULL)
 		{
 			free(arr);
 			for (j = 0; j <= i; j++)
