8b43c7ee387ca370db3098bf640426dbfdba48a1
AbouN <abounyangado@gmail.com>
2023-04-18 17:23:49 +0000

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * *malloc_checked -> allocates memory using malloc
  * @b: memory
@@ -10,9 +11,10 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
+
 	if (p == NULL)
 	{
 		exit(98);
 	}
-return (p);
+	return (p);
 }
